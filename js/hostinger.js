f7790bf80ee51e34129199781e6dbfac273702eb
uttampun <uttampun62@.com>
2022-10-16 21:43:13 +0545

    hero section complete

---
+++ js/hostinger.js
@@ -0,0 +1,22 @@
+
+function modal(){
+   document.getElementById('modal').style.display = "block";
+  
+}
+
+window.onload = function(){
+    setTimeout(modal, 5000);
+}
+window.onclick = function(){
+  document.getElementById('modal').style.display = "none"; 
+}
+
+window.onscroll = function(){scroll()}
+
+function scroll(){
+  if(document.body.scroll > 20 || document.documentElement.scrollTop > 20){
+     document.getElementsByClassName('nav-bar').style.top = "0";
+  }else{
+    document.getElementsByClassName('nav-bar').style.top = "50px";
+  }
+}
