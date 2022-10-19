d43d9ec81f2c28ca62a8c42ec939467e2b58b9db
uttampun <uttampun62@.com>
2022-10-19 09:35:59 +0545

    javascript file added

--- js/hostinger.js
+++ js/hostinger.js
@@ -1,25 +1,27 @@
 
+// modal
 function modal(){
    document.getElementById('modal').style.display = "block";
   
-}
+};
 
+// modal pop after 5 seconds
 window.onload = function(){
     setTimeout(modal, 5000);
-}
+};
+
+// pop up modal click outside
 window.onclick = function(){
   document.getElementById('modal').style.display = "none"; 
-}
+};
 
+// navbar scrolling
+window.addEventListener('scroll', () =>{
+  let scroll = document.querySelector('.nav-bar');
+  scroll.classList.toggle('sticky', window.scrollY >50);
+  let login = document.getElementById('login');
+  login.classList.toggle('login', window.scrollY >50);
+  
+  
+});
 
-
-// function scroll(){
-//   if(document.body.scroll > 20 || document.documentElement.scrollTop > 20){
-//      document.getElementsByClassName('nav-bar').style.top = "0";
-//   }else{
-//     document.getElementsByClassName('nav-bar').style.top = "50px";
-//   }
-// }
-
-
- 
